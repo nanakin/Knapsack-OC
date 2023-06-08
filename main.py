4699400f3168aad7380050e720cd46816586b3b0
Anna Kaczorowski <kaczoro.anna@gmail.com>
2023-06-08 17:43:19 +0200

    use share name as dataset keys + print results

--- main.py
+++ main.py
@@ -4,15 +4,25 @@ from algorithms import algorithms_list
 
 
 def read_dataset_file(dataset_filename):
+    dataset = {}
     with open(dataset_filename) as csv_file:
         csv_reader = DictReader(csv_file)
-        return list(csv_reader)
+        for row in csv_reader:
+            dataset[row["name"]] = {"price": float(row["price"]), "profit": float(row["profit"])}
+    return dataset
+
+
+def print_results(investments, total_cost, total_return):
+    print("Investments:")
+    for investment in investments:
+        print(investment)
+    print(f"\nTotal cost: {total_cost:.2f}€\nTotal return: {total_return:.2f}€")
 
 
 def main(algorithm_choice, dataset_filename):
     dataset = read_dataset_file(dataset_filename)
     results = algorithms_list[algorithm_choice](dataset)
-    print(*results)
+    print_results(*results)
 
 
 if __name__ == "__main__":
